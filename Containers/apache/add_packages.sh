a019d7e9f2c159af4dbab3f2522881d6c1903ad5
Michele Bologna <michele.bologna@suse.com>
2018-05-23 18:08:05 +0200

    Feat: add containers templates

---
+++ Containers/apache/add_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+zypper --non-interactive --gpg-auto-import-keys ref
+
+zypper --non-interactive in aaa_base aaa_base-extras net-tools timezone vim less sudo tar python python-xml apache2 apache2-prefork
