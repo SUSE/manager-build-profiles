0571f00963daea76c5fe796560688daaf6c4d263
Michele Bologna <michele.bologna@suse.com>
2022-01-25 14:48:19 +0100

    Feat: restore SLE15 packages

--- Containers/apache/add_packages.sh
+++ Containers/apache/add_packages.sh
@@ -3,4 +3,4 @@ set -e
 
 zypper --non-interactive --gpg-auto-import-keys ref
 
-zypper --non-interactive in aaa_base aaa_base-extras net-tools timezone python apache2 apache2-prefork
+zypper --non-interactive in aaa_base aaa_base-extras net-tools timezone vim less sudo tar python apache2 apache2-prefork
